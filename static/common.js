7167d886d294a64c726ad280aa65799774b3c8ea
juliend2 <juliend2@ubuntu.(none)>
2009-12-20 14:23:43 -0500

    supprimer le template pour ajouter des questions et mettre le form dans la page INDEX

--- static/common.js
+++ static/common.js
@@ -1,6 +1,15 @@
 // answers :
 var addanswer = {
 	init: function(){
-		
+		$('li.question>form').hide();
+		$('li.question>a.newanswer').toggle(function(){
+			// open
+			$(this).siblings('form').show();
+			return false;
+		},function(){
+			// close
+			$(this).siblings('form').hide();
+			return false;
+		});
 	}
 }
